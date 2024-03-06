f4693223000ac0499c0f4503f2e4813c53428c82
Sanoth-Repliq <sanoth.debnath@repliq.dev>
2024-03-06 20:33:34 +0600

    user and shop side completed

--- projectile/shopio/rest/urls/shop.py
+++ projectile/shopio/rest/urls/shop.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from shopio.rest.views.shop import ShopView
+from shopio.rest.views.shop import ShopView, ShopDetailsView
 
 urlpatterns = [
+    path("<uuid:shop_uid>", ShopDetailsView.as_view(), name="shop.details"),
     path("create", ShopView.as_view(), name="shop.create"),
 ]
--- projectile/shopio/rest/views/shop.py
+++ projectile/shopio/rest/views/shop.py
@@ -1,5 +1,7 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
+from rest_framework.exceptions import NotFound
 
+from shopio.models import Shop
 from shopio.rest.serializers.shop import ShopSerializer
 
 from shared.permission import IsShopOwner
@@ -8,3 +10,15 @@ from shared.permission import IsShopOwner
 class ShopView(CreateAPIView):
     serializer_class = ShopSerializer
     permission_classes = [IsShopOwner]
+
+
+class ShopDetailsView(RetrieveUpdateAPIView):
+    serializer_class = ShopSerializer
+    permission_classes = [IsShopOwner]
+
+    def get_object(self):
+        shop_uid = self.kwargs.get("shop_uid")
+        try:
+            return Shop.objects.get(uid=shop_uid)
+        except Shop.DoesNotExist:
+            raise NotFound("Shop not found")
