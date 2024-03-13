8297071ec160a55f65a0473beed964a7c2d6c16a
Sanoth-Repliq <sanoth.debnath@repliq.dev>
2024-03-13 21:20:58 +0600

    migrate files updated

--- deploy.sh
+++ deploy.sh
@@ -14,18 +14,29 @@ log "Pulling latest changes from Git..."
 git pull origin main
 log "Changes pulled successfully."
 
+log "activate virtualnev"
+source venv/bin/activate
+
 log "Clearing cache..."
+cd projectile
 python manage.py clean_pyc
 python manage.py clear_cache
 
-log "Installing dependencies..."
-pip install -r requirements.txt --no-input
 
-log "Collecting static files..."
-python manage.py collectstatic --noinput
+log "Installing dependencies..."
+pip install -r ../requirements/development.txt --no-input
+
+# log "Collecting static files..."
+# python manage.py collectstatic --noinput
+
+log "Running database makemigrations."
+python manage.py makemigrations
 
 log "Running database migration..."
 python manage.py migrate
+
+log "virualenv deactivate"
+deactivate
 
 log "Reloading app..."
 ps aux | grep gunicorn | grep ecommerce-backend | awk '{ print $2 }' | xargs kill -HUP
